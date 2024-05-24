5b6448ad61aaf6e98d1aa2153011c9c978a2a4c7
user <hersh1409@gmail.com>
2024-05-24 15:45:46 +0500

    fix

--- src/main/java/uz/jurayev/dynamic_document_excel/config/ObjectMapperConfig.java
+++ src/main/java/uz/jurayev/dynamic_document_excel/config/ObjectMapperConfig.java
@@ -1,9 +1,11 @@
 package uz.jurayev.dynamic_document_excel.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 @Configuration
@@ -11,10 +13,13 @@ public class ObjectMapperConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
+
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.setDateFormat(new SimpleDateFormat("dd-MM-yyyy HH:mm:ss"));
-
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+//        objectMapper.setDateFormat(dateFormat);
 
         return objectMapper;
     }
